95aaaf061ee4a2da75e0c9c5518bd0689390bf13
Sunidhi30 <sunidhi1408.be21@chitkara.edu.in>
2025-07-07 17:05:30 +0530

    added

--- src/app/(admin)/(others-pages)/(transactions)/transactionsall/page.tsx
+++ src/app/(admin)/(others-pages)/(transactions)/transactionsall/page.tsx
@@ -325,7 +325,7 @@ export default function TransactionsPage() {
             <motion.button
               onClick={handleOpenWithdrawalForm}
               disabled={isButtonDisabled}
-              className={`font-semibold py-2 px-6 rounded-lg shadow-lg transition-all duration-200 ${
+              className={`font-semibold py-2 px-9 rounded-lg shadow-lg transition-all duration-200 ${
                 isButtonDisabled
                   ? 'bg-gray-400 text-gray-200 cursor-not-allowed opacity-60'
                   : 'bg-blue-600 hover:bg-blue-700 text-white hover:shadow-xl'
